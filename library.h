52f5384608494511492e92c7926d64466206f468
Bhushan Kale <bhushanpkale@gmail.com>
2020-10-11 19:34:34 +0530

    skeleton code

---
+++ library.h
@@ -0,0 +1,32 @@
+#ifndef _LIBRARY_H
+#define _LIBRARY_H
+
+struct user {
+
+};
+
+struct book {
+
+};
+
+struct bookcopy {
+
+};
+
+struct issuerecord {
+
+};
+
+struct payment {
+
+};
+
+// owner functions
+
+// librarian functions
+
+// member functions
+
+// common functions
+
+#endif
---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "library.h"
+
+int main() {
+	printf("Hello Library!\n");
+	return 0;
+}
