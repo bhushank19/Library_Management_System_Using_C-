34a90f517b3ff970b7e5ef2b9bf6f43ef69f37a3
Bhushan Kale <bhushanpkale@gmail.com>
2020-10-11 19:38:17 +0530

    date operation

--- main.c
+++ main.c
@@ -2,8 +2,20 @@
 #include <stdlib.h>
 #include <string.h>
 #include "library.h"
+#include "date.h"
+
+void date_tester() {
+	date_t d1 = {1, 1, 2000}, d2 = {31, 12, 2000};
+	date_t d = {1, 1, 2000};
+	date_t r = date_add(d, 366);
+	date_print(&r);
+	int diff = date_cmp(d1, d2);
+	printf("date diff: %d\n", diff);
+}
+
 
 int main() {
 	printf("Hello Library!\n");
+    date_tester();
 	return 0;
 }
